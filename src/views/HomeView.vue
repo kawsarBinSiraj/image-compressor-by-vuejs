cb5481e2f65f98dd1253e950438e6c1c4d10e630
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2023-07-31 16:31:25 +0600

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,7 +27,6 @@ function onDrop(files, rejectReasons) {
     new Compressor(selectedFile, {
         quality: imgQuality.value / 10,
         convertTypes: ["image/jpg", "image/png", "image/jpeg"],
-        convertSize: Infinity,
         // The compression process is asynchronous,
         // which means you have to access the `result` in the `success` hook function.
         success(result) {
